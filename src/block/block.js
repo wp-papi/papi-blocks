b3e043dffbbc3e95a963e7ac4df76d3b105c3f0d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-05-07 15:40:00 +0200

    Restructure block file

--- src/block/block.js
+++ src/block/block.js
@@ -8,52 +8,48 @@ import DangerousHTML from 'react-dangerous-html';
 const { registerBlockType } = wp.blocks;
 
 /**
- * Register block box.
+ * Register block.
  *
- * @param {object} box
+ * @param {object} block
  */
-const registerBox = box => {
-	const id = box.id.replace(/_/g, '-').substr(1);
+const registerBlock = block => {
+	const settings = block;
 
-	registerBlockType( 'papi/' + id, {
-		title: box.title,
-		icon: box.icon,
-		category: typeof box.category === 'string' ? box.category : 'common',
-		keywords: [], // todo
+	/**
+	 * The edit function describes the structure of your block in the context of the editor.
+	 * This represents what the editor will render when the block is used.
+	 *
+	 * The "edit" property must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
+	settings.edit = (props) => {
+		return (
+			<div className={ props.className }>
+				<h2>{block.title}</h2>
+				<DangerousHTML html={block.html} />
+			</div>
+		);
+	};
 
-		/**
-		 * The edit function describes the structure of your block in the context of the editor.
-		 * This represents what the editor will render when the block is used.
-		 *
-		 * The "edit" property must be a valid function.
-		 *
-		 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-		 */
-		edit: function( props ) {
-			return (
-				<div className={ props.className }>
-					<h2>{box.title}</h2>
-					<DangerousHTML html={box.html} />
-				</div>
-			);
-		},
+	/**
+	 * The save function defines the way in which the different attributes should be combined
+	 * into the final markup, which is then serialized by Gutenberg into post_content.
+	 *
+	 * The "save" property must be specified and must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
+	settings.save = (props) => {
+		return (
+			<div>
+				<p>Hello from the frontend.</p>
+			</div>
+		);
+	};
 
-		/**
-		 * The save function defines the way in which the different attributes should be combined
-		 * into the final markup, which is then serialized by Gutenberg into post_content.
-		 *
-		 * The "save" property must be specified and must be a valid function.
-		 *
-		 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-		 */
-		save: function( props ) {
-			return (
-				<div>
-					<p>Hello from the frontend.</p>
-				</div>
-			);
-		},
-	} );
+	// Register block type.
+	registerBlockType( block.name, settings );
 };
 
 domReady(() => {
@@ -65,6 +61,6 @@ domReady(() => {
 
 	// Fetch boxes that should act like blocks.
 	apiFetch({ path: '/papi-ajax/?action=get_blocks&entry_type=' + entryType.value }).then(blocks => {
-		blocks.forEach(registerBox);
+		blocks.forEach(registerBlock);
 	});
 });
